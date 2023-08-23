b7eda1c511840565d27276e10984e351dfb1a82e
Samuel Sabino <sabinodasilvasamuel@gmail.com>
2023-08-23 07:21:43 -0300

    fix: update in routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+
 });
+Route::get('/results', 'WeatherController@search')->name('weather.search');
+

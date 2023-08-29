b0ac547398cb347cc9121bb1df9721e2f2258d36
Samuel Sabino <sabinodasilvasamuel@gmail.com>
2023-08-28 22:05:35 -0300

    fix: update css at all pages

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -2,11 +2,14 @@
 <html>
 <head>
     <title>Resultado da Previsão do Tempo</title>
+    <link href="{{ asset('welcome.css') }}" rel="stylesheet">
 </head>
-<body>
-    <h1>Previsão do Tempo para {{ $data['data'][0]['city_name'] }}, {{ $data['data'][0]['state_code'] }}</h1>
+<body class="weather">
+    <p>Previsão do Tempo para <strong>{{ $data['data'][0]['city_name'] }}</strong></p>
 
     <p>Temperatura: {{ $data['data'][0]['temp'] }}°C</p>
     <p>Condição: {{ $data['data'][0]['weather']['description'] }}</p>
+
+    <button onclick="window.location.href='/'">Nova pesquisa</button>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,12 @@
         <title>Previsão do tempo</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="{{ asset('welcome.css') }}" rel="stylesheet">
     </head>
-    <body>
-        <h1>Previsão do tempo</h1>
-        <form method="GET" action="{{ route('weather.search') }}">
+
+    <body id="weather">
+        <p>Consulte a temperatura da sua cidade, em tempo real </p>
+        <form class="weather_form" method="GET" action="{{ route('weather.search') }}">
             <input type="text" name="city" placeholder="Digite o nome da cidade">
             <button type="submit">Pesquisar</button>
         </form>
